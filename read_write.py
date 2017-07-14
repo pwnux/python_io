8c73da850cd13fe298d2d5d1000d32d5436a45e0
do duc huy <walter@trueplus.vn>
2017-07-14 18:40:48 +0700

    walter - commit source

---
+++ read_write.py
@@ -0,0 +1,9 @@
+raw_file_object = open("raw_file.txt", "r")
+
+# read the lines
+raw_file_lines = raw_file_object.readlines()
+# print raw_file_lines
+
+# read a line
+for elm in raw_file_lines:
+    print elm.split("\"")[1]
