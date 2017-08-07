354d032c72f13017fd358932d6fadd07d3f1d858
tuananhsn95 <tuananhsn95@gmail.com>
2017-08-07 21:27:51 +0700

    Fix raw_input => input

--- TestEnd.py
+++ TestEnd.py
@@ -1,23 +1,22 @@
 if __name__ == "__main__":
     SumLai = 0
     Moneymongmuon = 0
-    Moneyht = raw_input("Nhap so tien hien co: ")
+    Moneyht = input("Nhap so tien hien co: ")
     check = True
     while check:
-        Moneymongmuon = raw_input("Nhap so tien mong muon: ")
+        Moneymongmuon = input("Nhap so tien mong muon: ")
         if Moneymongmuon>Moneyht:
             check = False
         else:
             print "Nhap lai so tien mong muon, phai lon hon"
-    Laixuat = raw_input("Nhap lai xuat: ")
+    Laixuat = input("Nhap lai xuat: ")
     thang = 0
     while Moneyht<Moneymongmuon:
-
         tmp = Moneyht
-        temp = int(float(Moneyht)*float(Laixuat))
-        Moneyht = int(int(Moneyht) + int(int(temp)/100))
-        SumLai = int(SumLai) + int(Moneyht) - int(tmp)
+        temp = Moneyht*float(Laixuat)
+        Moneyht = int(Moneyht) + (Moneyht*float(Laixuat))/100
+        SumLai = SumLai + Moneyht - tmp
         thang += 1
     print "Tong lai la: " + str(SumLai)
-    print "Tong so tien la: " +str(int(Moneyht)+int(SumLai))
-    print "So thang la: "+str(thang)
+    print "Tong so tien la: " +str(Moneyht+SumLai)
+    print "So thang la: "+str(thang) +"thang"
